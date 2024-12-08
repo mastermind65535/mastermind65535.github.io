a8f93dc32380e3b7db683d4fbe629315d03f91ca
Mastermind <mastermind65535@gmail.com>
2024-12-08 20:06:40 +1300

    Project Viewer

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(detailsId) {
+    const details = document.getElementById(detailsId);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
